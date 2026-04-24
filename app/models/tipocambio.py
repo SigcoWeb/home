7db201ba7ca18ca0ac526ebd5b01e589f1090693
SigcoWeb <sigcoweb@gmail.com>
2026-04-24 12:29:11 -0500

    S
    S

--- app/models/tipocambio.py
+++ app/models/tipocambio.py
@@ -1,19 +1,20 @@
-from sqlalchemy import Column, Integer, String, DateTime, Date, Numeric
+from sqlalchemy import Column, Integer, Date, Numeric, Text, String, DateTime
 from datetime import datetime
 from app.database import Base
 
-class TipoCambio(Base):
+
+class SgcTipoCambio(Base):
     __tablename__ = "sgc_tipocambio"
 
     id_tc = Column(Integer, primary_key=True, index=True)
-    fecha_tc = Column(Date, nullable=False, unique=True)
-    compra = Column(Numeric(18, 3), nullable=True)
-    venta = Column(Numeric(18, 3), nullable=True)
-    compra_sunat = Column(Numeric(18, 3), nullable=True)
-    venta_sunat = Column(Numeric(18, 3), nullable=True)
-    nota = Column(String(100), nullable=True)
-    
-    # Audit
+    fecha_tc = Column(Date, nullable=False, unique=True, index=True)
+    compra = Column(Numeric(10, 3), nullable=False)
+    venta = Column(Numeric(10, 3), nullable=False)
+    compra_sunat = Column(Numeric(10, 3), default=0)
+    venta_sunat = Column(Numeric(10, 3), default=0)
+    nota = Column(Text, nullable=True)
+
+    # Auditoria
     id_usuario = Column(Integer, nullable=True)
     fhcontrol = Column(DateTime, default=datetime.now)
     estacion = Column(String(20), nullable=True)
